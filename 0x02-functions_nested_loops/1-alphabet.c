bb80824b09baebded5d24bb22c96ec76e28fdf33
Shaduj <olalemig14@gmail.com>
2023-06-21 15:41:15 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Check the code
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
