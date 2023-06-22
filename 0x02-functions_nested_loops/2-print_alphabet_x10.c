cecf682b00d646555c02cefb6dea5228dff4f114
Shaduj <olalemig14@gmail.com>
2023-06-21 17:49:44 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10 times.
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+
+	for (j = 1; i <= 10; i++)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
