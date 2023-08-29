c0605cbf5f4373a501a56a0eba2ee81e444e322e
Shaduj <olalemig14@gmail.com>
2023-08-28 21:34:24 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - prints striaght line 
+ * @n: no of timeto be printed
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else 
+	{
+		int i;
+
+		for (n = 1 ; i <= n i++)
+			_putchar('_');
+		_putchar('\n')
+	}
+}
