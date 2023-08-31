94d79bf4eaca963b87c87ccfe37bae1d895c746e
Shaduj <olalemig14@gmail.com>
2023-08-30 17:39:51 -0700

    recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function that return length of string
+ * @s: string to find length of sting
+ * Return: lengthof string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}
