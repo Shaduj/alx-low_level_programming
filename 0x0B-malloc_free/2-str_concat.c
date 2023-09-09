17c19ca92f342543f06aa2bc2a0172f29bcc90bc
Shaduj <olalemig14@gmail.com>
2023-09-08 22:44:43 -0700

    malloc

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * str_concat - concatenates two string
+ * @s1: first string
+ * @s2: second string
+ * Return: concatenated string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i;
+	char s1len;
+	char s2len;
+	char *contwo;
+
+	if (s1 == NULL)
+		return ("");
+	if (s2 == NULL)
+		return ("");
+	for (i = 0; s1[i] != '\0'; i++)
+		s1len++;
+	for (i = 0; s2[i] != '\0'; i++)
+		s2len++;
+
+	contwo = (char *)malloc(sizeof(char) * s1len + s2len + 1);
+
+	if (contwo == NULL)
+		return (NULL);
+	for (i = 0; s1[i] != '\0'; i++)
+		contwo[i] = s1[i];
+	for (i = 0; s2[i] != '\0'; i++)
+		contwo[s1len + i] = s2[i];
+	return (contwo);
+}
